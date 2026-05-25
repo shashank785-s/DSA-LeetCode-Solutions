ce45a7e6fc286bf7b2feb14a9dac577073b81013
Shashank Shekhar <shashankkr785@gmail.com>
2026-05-25 22:24:47 +0530

    Day 8: String to Integer (atoi)

---
+++ Arrays/String to Integer.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+   int myAtoi(string s) {
+       int i = 0, n = s.size();
+       int sign = 1;
+       long result = 0;
+
+       while (i < n && s[i] == ' ')
+           i++;
+
+       if (i < n && (s[i] == '+' || s[i] == '-')) {
+           sign = (s[i] == '-') ? -1 : 1;
+           i++;
+       }
+
+       while (i < n && isdigit(s[i])) {
+           result = result * 10 + (s[i] - '0');
+
+           if (result * sign > INT_MAX) return INT_MAX;
+           if (result * sign < INT_MIN) return INT_MIN;
+
+           i++;
+       }
+
+       return (int)(result * sign);
+   }
+};
